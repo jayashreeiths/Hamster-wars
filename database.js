6430b39c2491ad9a9ce83e7a1552e82a90dccd59
Jayashree bathala <71385982+jayashreeiths@users.noreply.github.com>
2021-04-26 10:53:06 +0200

    changed database.js

--- database.js
+++ database.js
@@ -1,6 +1,13 @@
 var admin = require("firebase-admin");
+let serviceAccount;
+if (process.env.PRIVATE_KEY) {
+    // På Heroku
+    serviceAccount = JSON.parse(process.env.PRIVATE_KEY)
+} else {
+    // Lokalt (på min dator)
+    serviceAccount = require("./hamster-wars-privatekey.json");
+}
 
-const serviceAccount = require("./hamster-wars-privatekey.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
